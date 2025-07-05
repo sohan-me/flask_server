8394002e9103692b3651d4143f4052ebf09e9abd
sohan-me <sohun.me@gmail.com>
2025-07-06 03:09:26 +0600

    Initial COmmit 03

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 import hashlib
 import json
+from flask_cors import CORS
 import os
 from datetime import datetime, timedelta
 from functools import wraps
